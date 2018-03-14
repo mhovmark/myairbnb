fdbfbe287ff458d907bb1e2cff7c3b2060c63eaf
Zartei <Zirdii@gmail.com>
2018-03-14 11:32:01 +0100

    TryToConnect via mysql

--- AirBnbProject/AirBnbProject/Form1.cs
+++ AirBnbProject/AirBnbProject/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace AirBnbProject
 {
@@ -18,6 +19,35 @@ namespace AirBnbProject
             /*
              * THis is my change
              */
+           
+
+            string server = "mydb8.surf-town.net";
+            string user = "hdb64894_handin3";
+            string pw = "teamhandin3";
+            string connStr = $"server={server};user={user};database=hb64894_myNHLdatabase;password={pw};";
+            MessageBox.Show(connStr.ToString());
+            MySqlConnection conn = new MySqlConnection(connStr);
+            Boolean connected = false;
+            try
+            {
+                conn.Open();
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            conn.Close();
+            if (connected)
+            {
+                MessageBox.Show("Correct login");
+
+            }
+            else
+            {
+                MessageBox.Show("Incorrect login");
+
+            }
         }
     }
 }
---
+++ AirBnbProject/AirBnbProject/dataHandlers.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace AirBnbProject
+{
+    class Country
+    {
+        private string country;
+        private int population;
+        private int bnbPerCap;
+        private List<City> city;
+
+        public Country(List<City> city)
+        {
+
+        }
+    }
+
+    class City
+    {
+        private string cityName;
+        private int population;
+        private int avgIncome;
+        private int turistYear;
+        private List<Accommodations> Accommodations;
+
+        public City(List<Accommodations> Accommodations)
+        {
+
+        }
+    }
+
+    class Accommodations // Boende 
+    {
+        private int room_id;
+        private int host_id;
+        private string room_type;
+        private string borough;
+        private string neighbourhood;
+        private int reviews;
+        private double overall_satisfaction;
+        private int Accomodates;
+        private int bedrooms;
+        private int price;
+        private int minstay;
+        private double latitude;
+        private double longitude;
+        private string last_modifified;
+
+        public Accommodations()
+        {
+
+        }
+    }
+    /*
+    class Connection
+    {
+        static string server = "mydb8.surf-town.net";
+        static string user = "hdb64894_handin3";
+        static string pw = "teamhandin3";
+        private MySqlConnection conn;
+        string connStr = $"server={server};user={user};database=hb64894_airbnb;port=3306;password={pw};";
+        
+        public Connection()
+        {
+            conn = new MySqlConnection(connStr);
+        }
+
+        public MySqlConnection GetConn()
+        {
+            return conn;
+        }
+    }
+    */
+}
